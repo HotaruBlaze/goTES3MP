80f7330e621ded8db0f16b0e27985ec153ef7ac7
Phoenix <DeriousHD@gmail.com>
2020-07-11 04:35:15 +0100

    Confirm player exists before decrementing counter

--- src/eventHandler.go
+++ src/eventHandler.go
@@ -20,13 +20,9 @@ type PlayerStruct struct {
 
 func tes3mpOutputHandler(s string) {
 
-	// initHandler(Context)
-	// initHandler(&Context)
-
 	// debugToggle := viper.GetBool("debug")
 	enable_ServerOutput := viper.GetBool("enable_ServerOutput")
 	if enable_ServerOutput {
-		// This shouldnt not contain tes3mp log
 		fmt.Println(s)
 	}
 
@@ -43,8 +39,6 @@ func tes3mpOutputHandler(s string) {
 			log.Infoln(tes3mpLogMessage, "TES3MP Version:", dedicatedServerInfo[3], build)
 			TES3MPVersion = dedicatedServerInfo[3] + " " + build
 		}
-		// log.Infoln(tes3mpLogMessage, "TES3MPBuild:", build)
-		// TES3MPBuild = build
 
 	}
 	isTrue, _ = regexp.MatchString(`\[User\]`, s)
@@ -58,13 +52,18 @@ func tes3mpOutputHandler(s string) {
 			log.Infoln(tes3mpLogMessage, "Player", player.Name, player.Responce)
 			CurrentPlayers = CurrentPlayers + 1
 			Players = AppendIfMissing(Players, player.Name)
-			DiscordSendMessage("[EventHandler] " + player.Name + " joined the server.")
+			DiscordSendMessage("[TES3MP] " + player.Name + " joined the server.")
 
 		case "Disconnected":
 			log.Infoln(tes3mpLogMessage, "Player", player.Name, player.Responce)
-			CurrentPlayers = CurrentPlayers - 1
-			Players = RemoveEntryFromArray(Players, player.Name)
-			DiscordSendMessage("[EventHandler] " + player.Name + " left the server.")
+			_, found := FindinArray(Players, player.Name)
+			if found {
+				CurrentPlayers = CurrentPlayers - 1
+				Players = RemoveEntryFromArray(Players, player.Name)
+				DiscordSendMessage("[TES3MP] " + player.Name + " left the server")
+			} else {
+				DiscordSendMessage("[TES3MP] " + player.Name + " left the server, But we never got their connected")
+			}
 		}
 	}
 	// isTrue, _ = regexp.MatchString(`\[Chat\]`, s)
